b2bb1c1b1d98940cf2b9691d35dc4f79815c8e2d
RexO77 <nischal.skanda07@gmail.com>
2024-11-22 17:19:27 +0530

    Added Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const WallpaperApp());
@@ -13,7 +13,7 @@ class WallpaperApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wallpaper Showcase',
+      title: 'Wallpaper Carousel',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
@@ -22,106 +22,50 @@ class WallpaperApp extends StatelessWidget {
   }
 }
 
-class WallpaperHomePage extends StatefulWidget {
+class WallpaperHomePage extends StatelessWidget {
   const WallpaperHomePage({super.key});
 
   @override
-  State<WallpaperHomePage> createState() => _WallpaperHomePageState();
-}
-
-class _WallpaperHomePageState extends State<WallpaperHomePage> with SingleTickerProviderStateMixin {
-  final List<String> wallpapers = [
-    'https://example.com/wallpaper1.jpg',
-    'https://example.com/wallpaper2.jpg',
-    // Add more URLs
-  ];
-
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _openWallpaper(String url) {
-    // Implement the function to open the wallpaper details page
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final List<String> wallpapers = [
+      'assets/mobilewalls/1.jpg',
+      'assets/mobilewalls/2.jpeg',
+      'assets/mobilewalls/3.jpg',
+      'assets/mobilewalls/4.jpeg',
+      'assets/mobilewalls/5.jpeg',
+      'assets/mobilewalls/6.jpeg',
+      'assets/mobilewalls/7.jpeg',
+      // Add more image paths
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wallpaper Showcase'),
+        title: const Text('Wallpaper Carousel'),
         centerTitle: true,
-        leading: const Icon(Icons.menu),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-        ],
       ),
-      body: FadeTransition(
-        opacity: _animation,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: MasonryGridView.count(
-            crossAxisCount: 2,
-            itemCount: wallpapers.length,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () => _openWallpaper(wallpapers[index]),
-                child: Hero(
-                  tag: wallpapers[index],
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(wallpapers[index]),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
+      body: CarouselSlider(
+        options: CarouselOptions(
+          height: MediaQuery.of(context).size.height * 0.6,
+          autoPlay: true,
+          enlargeCenterPage: true,
+        ),
+        items: wallpapers.map((item) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    item,
+                    fit: BoxFit.cover,
+                    width: MediaQuery.of(context).size.width,
                   ),
                 ),
               );
             },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class WallpaperDetailPage extends StatelessWidget {
-  final String imageUrl;
-  final String tag;
-
-  const WallpaperDetailPage({super.key, required this.imageUrl, required this.tag});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Wallpaper Detail'),
-      ),
-      body: Center(
-        child: Hero(
-          tag: tag,
-          child: Image.network(imageUrl),
-        ),
+          );
+        }).toList(),
       ),
     );
   }
