be9835e41a41dc7f3c56da4be294a54ad508877d
RexO77 <nischal.skanda07@gmail.com>
2024-11-22 17:02:52 +0530

    App Trials

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() {
-  runApp(WallpaperApp());
+  runApp(const WallpaperApp());
 }
 
 class WallpaperApp extends StatelessWidget {
@@ -16,73 +17,89 @@ class WallpaperApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: WallpaperHomePage(),
+      home: const WallpaperHomePage(),
     );
-  }//comment
+  }
 }
 
 class WallpaperHomePage extends StatefulWidget {
   const WallpaperHomePage({super.key});
 
   @override
-  _WallpaperHomePageState createState() => _WallpaperHomePageState();
+  State<WallpaperHomePage> createState() => _WallpaperHomePageState();
 }
 
-class _WallpaperHomePageState extends State<WallpaperHomePage> {
-  // Sample list of wallpaper URLs
+class _WallpaperHomePageState extends State<WallpaperHomePage> with SingleTickerProviderStateMixin {
   final List<String> wallpapers = [
     'https://example.com/wallpaper1.jpg',
     'https://example.com/wallpaper2.jpg',
     // Add more URLs
   ];
 
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _openWallpaper(String url) {
+    // Implement the function to open the wallpaper details page
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wallpaper Showcase'),
+        title: const Text('Wallpaper Showcase'),
         centerTitle: true,
-        leading: Icon(Icons.menu),
+        leading: const Icon(Icons.menu),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
         ],
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: wallpapers.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Two images per row
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              // Navigate to detail view with animation
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => WallpaperDetailPage(
-                    imageUrl: wallpapers[index],
-                    tag: 'wallpaper$index',
+      body: FadeTransition(
+        opacity: _animation,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: MasonryGridView.count(
+            crossAxisCount: 2,
+            itemCount: wallpapers.length,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () => _openWallpaper(wallpapers[index]),
+                child: Hero(
+                  tag: wallpapers[index],
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(wallpapers[index]),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                   ),
                 ),
               );
             },
-            child: Hero(
-              tag: 'wallpaper$index',
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(wallpapers[index]),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-            ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
@@ -92,7 +109,7 @@ class WallpaperDetailPage extends StatelessWidget {
   final String imageUrl;
   final String tag;
 
-  WallpaperDetailPage({super.key, required this.imageUrl, required this.tag});
+  const WallpaperDetailPage({super.key, required this.imageUrl, required this.tag});
 
   @override
   Widget build(BuildContext context) {
