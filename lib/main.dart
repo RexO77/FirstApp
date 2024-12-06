2ef963c53a40415122e0bb149d57889d76974795
RexO77 <nischal.skanda07@gmail.com>
2024-12-06 23:25:21 +0530

    App Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'dart:ui'; // For glassmorphic effect
 import 'dart:io';
@@ -16,12 +16,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       title: 'ArtistryWalls',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.indigo,
-        fontFamily: 'Roboto', // Use a custom font
+      theme: const CupertinoThemeData(
+        brightness: Brightness.light,
+        primaryColor: CupertinoColors.activeBlue,
       ),
       home: const WallpaperHomePage(),
       debugShowCheckedModeBanner: false,
@@ -33,7 +32,6 @@ class WallpaperHomePage extends StatefulWidget {
   const WallpaperHomePage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _WallpaperHomePageState createState() => _WallpaperHomePageState();
 }
 
@@ -43,7 +41,6 @@ class _WallpaperHomePageState extends State<WallpaperHomePage>
   final Set<String> favorites = {};
   int _tapCounter = 0;
   DateTime _lastTapTime = DateTime.now();
-  late AnimationController animationController;
 
   final List<String> wallpapers = [
     'assets/mobilewalls/10.png',
@@ -65,30 +62,41 @@ class _WallpaperHomePageState extends State<WallpaperHomePage>
     // Add more image paths
   ];
 
-  @override
-  void initState() {
-    super.initState();
-      animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    );
-  }
-
   Future<void> _downloadImage(String assetPath) async {
     try {
       final byteData = await rootBundle.load(assetPath);
       final buffer = byteData.buffer;
-      final directory = await getExternalStorageDirectory();
-      final filePath = '${directory?.path}/${assetPath.split('/').last}';
+      final directory = await getApplicationDocumentsDirectory();
+      final filePath = '${directory.path}/${assetPath.split('/').last}';
       final file = File(filePath);
       await file.writeAsBytes(buffer.asUint8List(
           byteData.offsetInBytes, byteData.lengthInBytes));
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Image downloaded to $filePath')),
+      showCupertinoDialog(
+        context: context,
+        builder: (_) => CupertinoAlertDialog(
+          title: const Text('Download Successful'),
+          content: Text('Image saved to $filePath'),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
       );
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to download image')),
+      showCupertinoDialog(
+        context: context,
+        builder: (_) => CupertinoAlertDialog(
+          title: const Text('Download Failed'),
+          content: const Text('Unable to save image'),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
       );
     }
   }
@@ -121,180 +129,151 @@ class _WallpaperHomePageState extends State<WallpaperHomePage>
   Widget build(BuildContext context) {
     final List<Widget> pages = [
       // Home Page
-      Column(
-        children: [
-          Expanded(
-            child: CarouselSlider.builder(
-              itemCount: wallpapers.length,
-              itemBuilder: (context, index, realIndex) {
-                final image = wallpapers[index];
-                final isFavorite = favorites.contains(image);
-                return GestureDetector(
-                  onTap: () => _handleTap(image),
-                  child: Stack(
-                    children: [
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeInOut,
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black38,
-                              blurRadius: 10,
-                              offset: Offset(0, 5),
+      CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('ArtistryWalls'),
+          border: null,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: CarouselSlider.builder(
+                itemCount: wallpapers.length,
+                itemBuilder: (context, index, realIndex) {
+                  final image = wallpapers[index];
+                  final isFavorite = favorites.contains(image);
+                  return GestureDetector(
+                    onTap: () => _handleTap(image),
+                    child: Stack(
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: CupertinoColors.systemGrey,
+                                blurRadius: 10,
+                                offset: Offset(0, 5),
+                              ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Image.asset(
+                              image,
+                              fit: BoxFit.cover,
+                              width: MediaQuery.of(context).size.width,
                             ),
-                          ],
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.asset(
-                            image,
-                            fit: BoxFit.cover,
-                            width: MediaQuery.of(context).size.width,
                           ),
                         ),
-                      ),
-                      if (isFavorite)
-                        Positioned(
-                          top: 10,
-                          right: 10,
-                          child: Icon(
-                            Icons.favorite,
-                            color: Colors.redAccent,
-                            size: 30,
+                        if (isFavorite)
+                          const Positioned(
+                            top: 10,
+                            right: 10,
+                            child: Icon(
+                              CupertinoIcons.heart_fill,
+                              color: CupertinoColors.systemRed,
+                              size: 30,
+                            ),
                           ),
-                        ),
-                    ],
-                  ),
-                );
-              },
-              options: CarouselOptions(
-                scrollDirection: Axis.vertical,
-                autoPlay: true,
-                autoPlayInterval: const Duration(seconds: 5),
-                enlargeCenterPage: true,
-                viewportFraction: 0.8,
+                      ],
+                    ),
+                  );
+                },
+                options: CarouselOptions(
+                  scrollDirection: Axis.horizontal,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 5),
+                  enlargeCenterPage: true,
+                  viewportFraction: 0.8,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       // Categories Page (Placeholder)
-      Center(
-        child: Text(
-          'Categories',
-          style: Theme.of(context).textTheme.headlineMedium,
+      CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Categories'),
+          border: null,
+        ),
+        child: Center(
+          child: Text(
+            'Coming Soon',
+            style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+          ),
         ),
       ),
       // Favorites Page
-      GridView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: favorites.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Adjust as needed
-          childAspectRatio: 0.7,
+      CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Favorites'),
+          border: null,
         ),
-        itemBuilder: (context, index) {
-          final image = favorites.elementAt(index);
-          return Container(
-            margin: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.asset(
-                image,
-                fit: BoxFit.cover,
+        child: GridView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: favorites.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // Adjust as needed
+            childAspectRatio: 0.7,
+          ),
+          itemBuilder: (context, index) {
+            final image = favorites.elementAt(index);
+            return Container(
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                    color: CupertinoColors.systemGrey2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
               ),
-            ),
-          );
-        },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     ];
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: const Text('ArtistryWalls'),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Stack(
-        children: [
-          pages[_currentIndex],
-        ],
-      ),
-      bottomNavigationBar: GlassmorphicNavigationBar(
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
-      ),
-    );
-  }
-}
-
-class GlassmorphicNavigationBar extends StatelessWidget {
-  final int currentIndex;
-  final ValueChanged<int> onTap;
-
-  const GlassmorphicNavigationBar({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 70,
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.3),
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-          child: BottomNavigationBar(
-            currentIndex: currentIndex,
-            onTap: onTap,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white70,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_filled),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.category),
-                label: 'Categories',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'Favorite',
-              ),
-            ],
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.collections),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart),
+            label: 'Favorites',
+          ),
+        ],
       ),
+      tabBuilder: (context, index) {
+        return pages[index];
+      },
     );
   }
 }
